905918e19652dfe18a2273720860310feef324a8
Jéssica <jessicapmaximo@gmail.com>
2023-02-09 18:28:13 -0300

    component shopping cart criado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import './App.css';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/shoppingCart" component={ ShoppingCart } />
       <Route path="/" component={ Home } />
+
     </Switch>
   );
 }
