895f415fa7f83064ff6ee391499e6561bc615f38
Jéssica <jessicapmaximo@gmail.com>
2023-02-09 19:39:35 -0300

    requisitos 1 a 3 ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,18 @@ import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
-      </div>
+      <>
+
+        <div>
+          <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
+        </div>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </>
+
     );
   }
 }
