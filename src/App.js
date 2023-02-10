73cda2bc9695ea1049796fc3de29b68de71bffdf
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-02-10 18:29:03 -0300

    Requisito 6 OK

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Carrinho from './components/Carrinho';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route path="/cart" component={ Carrinho } />
+        <Route path="/cart" component={ Cart } />
       </Switch>
     );
   }
